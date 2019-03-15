166aec2031ea5b7c73775aef21641b4e04ecb67d
Geoff <geoff@localhost.localdomain>
2019-03-15 15:25:07 +0100

    add new component and route change'

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,28 +1,25 @@
 <template>
     <v-toolbar fixed class="purple darken-2" dark>
-        <img src="../assets/LogoALE.png" aspect-ratio="0.2">
+        <router-link to="/"><img src="../assets/LogoALE.png" aspect-ratio="0.2"></router-link>
       <v-toolbar-title class="mr-1">
-        <v-btn flat dark>
-          <router-link to="/">Sandbox</router-link>
-        </v-btn>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items>
-        <v-btn flat dark>
-         <router-link to="messageFAQ">
+        <v-btn flat dark
+         @click="navToMessageFAQ({name: 'messageFAQ'})">
            <v-icon x-large color="purple darken-5">email</v-icon>
-         </router-link>
+        </v-btn>
+      </v-toolbar-items>
+      <v-toolbar-items>
+        <v-btn flat dark
+          @click="navToSignIn({name: 'signin'})">
+          Sign in
         </v-btn>
       </v-toolbar-items>
       <v-toolbar-items>
         <v-btn flat dark
           @click="navToRegister({name: 'register'})">
-          Sign in
-        </v-btn>
-      </v-toolbar-items>
-      <v-toolbar-items>
-        <v-btn flat dark>
-          <router-link to="register">Register</router-link>
+          Register
         </v-btn>
       </v-toolbar-items>
     </v-toolbar>
@@ -31,12 +28,23 @@
 <script>
 export default {
   methods: {
+    navToSignIn (route) {
+      this.$router.push(route)
+    },
     navToRegister (route) {
+      this.$router.push(route)
+    },
+    navToMessageFAQ (route) {
+      this.$router.push(route)
+    },
+    navToHome (route) {
       this.$router.push(route)
     }
   }
 }
 </script>
+
+SandboxALE-AAPP_2019
 
 <style scoped>
 </style>
