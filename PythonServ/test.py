91e4a702070bb1b3610a3d612ce5311891c9494c
Geoff <geoff@localhost.localdomain>
2019-03-27 12:33:08 +0100

    Stable version

--- PythonServ/test.py
+++ PythonServ/test.py
@@ -1,6 +1,10 @@
-x = "hello world"
-print(x)
-x = "this sucks"
-print(x)
-print(32)
-print('\n')
+# x = "hello world"
+# print(x)
+# x = "this sucks"
+# print(x)
+# print(32)
+# print('\n')
+str = "test"
+ptr = "testok"
+strptr = str + " " + ptr
+print(strptr)
