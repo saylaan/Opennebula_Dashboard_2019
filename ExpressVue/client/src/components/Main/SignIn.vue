a3ab54b9e7b156944be204fc5a8c6622aedfd9dc
Huck Geoffroy <geoffroy.huck@al-enterprise.com>
2019-04-03 09:10:17 +0200

    merge

--- ExpressVue/client/src/components/Main/SignIn.vue
+++ ExpressVue/client/src/components/Main/SignIn.vue
@@ -33,7 +33,6 @@ export default {
           email: this.email,
           password: this.password
         })
-        console.log(response.data.user)
         this.$store.dispatch('setToken', response.data.token)
         this.$store.dispatch('setUser', response.data.user)
         this.$router.push({
