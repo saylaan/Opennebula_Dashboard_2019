82f8232dfcc50daaee0188b0ef60427be473d67f
root <root@portal-vlab.ale-aapp.com>
2019-07-05 15:29:58 +0200

    merge

--- server/src/password/HandlingPwd.js
+++ server/src/password/HandlingPwd.js
@@ -3,7 +3,7 @@ const Opennebula = require('opennebula')
 const one = new Opennebula('geoffroy:2961Sailaan1992!',
   'http://vlab.ale-aapp.com:2633/RPC2')
 const { exec } = require('child_process')
-const https = require('https')
+const http = require('http')
 
 const {
   Url,
@@ -26,23 +26,29 @@ module.exports = {
     }
   },
   async pwd02G(o2g) { // USE SCRIPT
-    const cmdSSH = "ssh oneadmin@vlab.ale-aapp.com '/var/lib/one/scripts/changeO2Gpasswd.sh "
-    + o2g.urltype.toUpperCase() + " " + o2g.password + "'"
-    exec(cmdSSH, (err, stdout, stderr) => {
+    const cmdSSH = "ssh oneadmin@10.1.2.150 /var/lib/one/scripts/changeO2Gpasswd.sh "
+    + o2g.urltype + " " + o2g.password
+/*    exec(cmdSSH, (err, stdout, stderr) => {
       if (err) {
         console.log(err)
         return
       }
       console.log(stdout)
-    })
+    })*/
   },
   async pwdSIP(sip) { // REQUEST HTTPS
-    const url = "https://o2g-" 
+    const url = "http://o2g-" 
     + sip.vlabname.toLowerCase() 
-    + ".ale-aapp.com/api/"
-    // + ".ale-aapp.com/api/rest/authenticate?version=1.0"
+    //+ ".ale-aapp.com/api/rest/"
+    + ".ale-aapp.com/api/rest/authenticate?version=1.0"
 
-    https.get(url, (res) => {
+    console.log('IIIIIIIIIIMMMMMMMMMMMMMMMMM HERRRRRRRRRREEEEEE')
+    console.log('IIIIIIIIIIMMMMMMMMMMMMMMMMM HERRRRRRRRRREEEEEE')
+    console.log(url, url, url)
+    console.log('IIIIIIIIIIMMMMMMMMMMMMMMMMM HERRRRRRRRRREEEEEE')
+    console.log('IIIIIIIIIIMMMMMMMMMMMMMMMMM HERRRRRRRRRREEEEEE')
+    console.log('IIIIIIIIIIMMMMMMMMMMMMMMMMM HERRRRRRRRRREEEEEE')
+    await http.get(url, (res) => {
       console.log('statusCode:', res.statusCode)
       console.log('headers:', res.headers)
       
