3d2cd2134aeca34c4a1ae372d1cf25d65ef7e60e
geoffroy huck <geoffroy.huck@epitech.eu>
2019-07-05 15:31:15 +0200

    new test

--- server/src/password/HandlingPwd.js
+++ server/src/password/HandlingPwd.js
@@ -3,7 +3,7 @@ const Opennebula = require('opennebula')
 const one = new Opennebula('geoffroy:2961Sailaan1992!',
   'http://vlab.ale-aapp.com:2633/RPC2')
 const { exec } = require('child_process')
-const https = require('https')
+const request = require('request')
 
 const {
   Url,
@@ -37,21 +37,50 @@ module.exports = {
     })
   },
   async pwdSIP(sip) { // REQUEST HTTPS
-    const url = "https://o2g-" 
-    + sip.vlabname.toLowerCase() 
-    + ".ale-aapp.com/api/"
-    // + ".ale-aapp.com/api/rest/authenticate?version=1.0"
-
-    https.get(url, (res) => {
-      console.log('statusCode:', res.statusCode)
-      console.log('headers:', res.headers)
-      
-      res.on('data', (d) => {
-        process.stdout.write(d)
-      })
-    }).on('error', (e) => {
-      console.error(e)
+    const urlAuthen = "https://o2g-" 
+    + sip.vlabname.toUpperCase() 
+    //+ ".ale-aapp.com/api/"
+    + ".ale-aapp.com"
+    const pathAuth = "/api/rest/authenticate?version=1.0"
+    const username = "admin"
+    const passwd = "none"
+    
+    const options = {
+      url : urlAuthen,
+      path: pathAuth,
+      method: 'GET',
+      port: 443,
+      headers: {
+        'Authorization': 'Basic' + new Buffer(username + ':' + passwd)
+        .toString('base64')
+      }
+    }
+    request.get(options, (err, res, html) => {
+      if (err) {
+        console.log(err)
+        return
+      }
+      console.log(res)
     })
+    console.log('IMMMM HEREEEEEEEE!!!')
+    console.log('IMMMM HEREEEEEEEE!!!')
+    console.log(urlAuthen)
+    console.log('IMMMM HEREEEEEEEE!!!')
+    console.log('IMMMM HEREEEEEEEE!!!')
+    console.log('IMMMM HEREEEEEEEE!!!')
+
+
+
+    // https.get(url, (res) => {
+    //   console.log('statusCode:', res.statusCode)
+    //   console.log('headers:', res.headers)
+      
+    //   res.on('data', (d) => {
+    //     process.stdout.write(d)
+    //   })
+    // }).on('error', (e) => {
+    //   console.error(e)
+    // })
     // console.log('new password sip', sip.passwd)
     // console.log('new password sip', sip.passwd)
   }
