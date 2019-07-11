3b6e21f0cdc17b2bb2731edc71b0e9bc9dcf15ef
geoffroy huck <geoffroy.huck@epitech.eu>
2019-07-11 12:26:48 +0200

    add test

--- server/src/password/HandlingPwd.js
+++ server/src/password/HandlingPwd.js
@@ -41,6 +41,7 @@ module.exports = {
   },
   async pwdSIP(sips) { // REQUEST HTTPS
     console.log(sips)
+
     const url = await Url.findOne({
       where: {
         urltype: sips[0].vlabname.toLowerCase()
@@ -53,45 +54,21 @@ module.exports = {
       path: "/api/rest",
       method: 'GET',
       port: 443,
+      // headers: {
+      //   'Authorization': 'Basic' + new Buffer("admin" + ':' + url.password)
+      //   .toString('base64')
+      // },
       //rejectUnauthorized: false
     }
-    http.get(options, (res) => {
-      let body = ""
+    let body = ""
+    await http.get(options, (res) => {
       res.on('data', (data) => {
         body += data
       })
-      res.on('end', async () => {
+      res.on('end', () => {
         if (body) {
-          console.log('body', body)
-          console.log('Trying to authenticate to the server O2G....')
-          console.log('inside pwdSIP', url.urltype, url.password)
-          console.log("https://o2g-" + sips[0].vlabname.toLowerCase() + ".ale-aapp.com")
-          options = {
-            hostname : "o2g-" + sips[0].vlabname.toLowerCase() + ".ale-aapp.com",
-            path: "/api/rest/authenticate?version=1.0",
-            method: 'GET',
-            port: 443,
-            headers: {
-              'Authorization': 'Basic' + new Buffer("admin" + ':' + url.password)
-              .toString('base64')
-            },
-            rejectUnauthorized: false
-          }
-          console.log(options.headers)
-          // await https.request(options, (res) => {
-          //   console.log("https://o2g-" + sips[0].vlabname.toLowerCase() + ".ale-aapp.com")
-          //   let body = ""
-          //   res.on('data', (data) => {
-          //     body += data
-          //   })
-          //   res.on('end', () => {
-          //     console.log(body)
-          //     console.log("The timeout has finished without any trouble")
-          //   })
-          //   res.on('error', (e) => {
-          //     console.log('error: ', e.message)
-          //   })
-          // })
+          console.log(body)
+          console.log('Trying to authenticate to the server ...')
         }
       })
       res.on('error', (e) => {
@@ -99,6 +76,33 @@ module.exports = {
       })
     })
     
+    // console.log('inside pwdSIP', url.urltype, url.password)
+    // console.log("https://o2g-" + sips[0].vlabname.toLowerCase() + ".ale-aapp.com")
+    // let options = {
+    //   host : "o2g-" + sips[0].vlabname.toLowerCase() + ".ale-aapp.com",
+    //   path: "/api/rest/authenticate?version=1.0",
+    //   method: 'GET',
+    //   port: 443,
+    //   headers: {
+    //     'Authorization': 'Basic' + new Buffer("admin" + ':' + url.password)
+    //     .toString('base64')
+    //   },
+    //   rejectUnauthorized: false
+    // }
+    // https.get(options, (res) => {
+    //   console.log("https://o2g-" + sips[0].vlabname.toLowerCase() + ".ale-aapp.com")
+    //   let body = ""
+    //   res.on('data', (data) => {
+    //     body += data
+    //   })
+    //   res.on('end', () => {
+    //     console.log(body)
+    //     console.log("The timeout has finished without any trouble")
+    //   })
+    //   res.on('error', (e) => {
+    //     console.log('error: ', e.message)
+    //   })
+    // })
   }
 }
 
