89388fbf3ad450b98ad6514468f5be2eba4037ce
geoffroy huck <geoffroy.huck@epitech.eu>
2019-07-12 16:14:47 +0200

    add test session co

--- server/src/password/HandlingPwd.js
+++ server/src/password/HandlingPwd.js
@@ -63,9 +63,7 @@ module.exports = {
       })
       res.on('end', () => {
         if (body) {
-          console.log(body)
           console.log('Got answer from o2g ', url.urltype, url.password)
-          console.log("https://o2g-" + sips[0].vlabname.toLowerCase() + ".ale-aapp.com")
           let options = {
             host : "o2g-" + sips[0].vlabname.toLowerCase() + ".ale-aapp.com",
             path: "/api/rest/authenticate?version=1.0",
@@ -78,17 +76,14 @@ module.exports = {
           }
           console.log('Basic auth token', options.headers)
           https.get(options, (res) => {
-            console.log("https://o2g-" + sips[0].vlabname.toLowerCase() + ".ale-aapp.com")
             let body = ""
             res.on('data', (data) => {
               body += data
             })
             res.on('end', () => {
-              console.log(body)
               const bodyArray = body.split(" ")
               let cookie = bodyArray[4]
               cookie = cookie.substr(1, cookie.length - 4)
-              console.log('cookie', cookie)
               console.log("The Authentification has finished without any trouble")
               const session = {
                 applicationName: 'PBXSession'
@@ -103,14 +98,16 @@ module.exports = {
                   'Content-Type': 'application/json',
                   'Cookie': 'AlcUserId=' + cookie
                 },
+                rejectUnauthorized: false
               }
+              console.log(options)
               https.get(options, (res) => {
-                console.log("https://o2g-" + sips[0].vlabname.toLowerCase() + ".ale-aapp.com")
                 let body = ""
                 res.on('data', (data) => {
                   body += data
                 })
                 res.on('end', () => {
+                  console.log('Success connection to the session')
                   console.log(body)
                 })
                 res.on('error', (e) => {
@@ -122,7 +119,7 @@ module.exports = {
             })
           })
         })
-      }//0388482405
+      }
       res.on('error', (e) => {
         console.log('error: ', e.message)
       })
