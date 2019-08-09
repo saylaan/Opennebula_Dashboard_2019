d94d8365afa396263e46de709ed9bdbe0db7661e
root <root@portal-vlab.ale-aapp.com>
2019-08-09 09:23:07 +0200

    mergeé

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,7 @@
 const imports = require('./config/imports') // all import for the app
 const { sequelize } = require('./models') // models folder with index.js file who return a sequelize obj
 //const publicRoot = `D:/Users/ghuck2671/Desktop/Geoff/SB/SandboxALE-AAPP_2019/client/dist` // ON WINDOWS
-// const publicRoot = `/root/SandboxALE-AAPP_2019/client/dist` // ON LINUX
+const publicRoot = `/root/SandboxALE-AAPP_2019/client/dist` // ON LINUX
 
 const key = imports.fs.readFileSync(__dirname + '/cert/selfsigned.key') // Certificat
 const cert = imports.fs.readFileSync(__dirname + '/cert/selfsigned.crt')
@@ -24,7 +24,7 @@ app.use(imports.cookieSession({
    keys: ['portal-vlab'],
    maxAge: 24 * 60 * 60 * 1000 // 24 hours
  }))
-// app.use(imports.express.static(publicRoot)) // For production (use of the build fodler for vue)
+app.use(imports.express.static(publicRoot)) // For production (use of the build fodler for vue)
 
 require('./policies/passport') // this is for passport authen
 require('./routes')(app) // attach all the different endpoint to the apps
@@ -42,17 +42,10 @@ const server = imports.https.createServer(options, app)
 sequelize.sync() // sync sequelize with the server {force : true} = deleting all data
   .then(() => imports.dbopenneb.populateDB())
   .then(() => {
-<<<<<<< HEAD
     // app.listen(imports.config.portHttps, imports.config.ip) // Adding the ip for change domain for production    
     // app.listen(imports.config.portHttps)
     // app.listen(imports.config.portHttp)
     server.listen(imports.config.portHttps, imports.config.ip)
-=======
-    //app.listen(imports.config.portHttps, imports.config.ip) // Adding the ip for change domain for production    
-    //app.listen(imports.config.portHttps)
-    app.listen(imports.config.portHttp)
-   //  server.listen(imports.config.portHttps, imports.config.ip)
->>>>>>> 2fdb144c59c00c648ee36ed9ad406962fd529763
     console.log('####################### END INIT DB #######################\n\n')
     console.log(`Server started on port ${imports.config.portHttp}... let's start working...`)
   })
