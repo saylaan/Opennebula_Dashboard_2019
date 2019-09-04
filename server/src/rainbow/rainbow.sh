81232d3433fdc11dbb6ed1fefac9fc6b69702ea5
Frank Gadot <33880848+fgc92@users.noreply.github.com>
2019-09-04 06:50:42 -0400

    Update rainbow.sh

--- server/src/rainbow/rainbow.sh
+++ server/src/rainbow/rainbow.sh
@@ -2,7 +2,7 @@
 
 function log()
 {
-	echo `date "+%Y.%m.%d %H:%M:%S"` "- $1" >> rainbow.log
+	echo `date "+%Y.%m.%d %H:%M:%S"` "- $1" >> /root/SandboxALE-AAPP_2019/server/src/rainbow/rainbow.log
 }
 
 log "$*"
