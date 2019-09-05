06d084722b8be27b4160aae036693921120cea48
Your Name <you@example.com>
2019-09-05 14:18:07 +0200

    test refresh work

--- server/src/policies/isAuthenticated.js
+++ server/src/policies/isAuthenticated.js
@@ -1,14 +1,16 @@
 const passport = require('passport')
 
 module.exports = function (req, res, next) {
-  passport.authenticate('jwt', function (err, user) {
-    if (err || !user) {
-      res.status(403).send({
-        error: 'you do not have access to this resource'
-      })
-    } else {
-      req.user = user
-      next()
-    }
+  passport.authenticate('jwt', async function (err, user) {
+    await setTimeout(async (err, data) => {
+      if (err || !user) {
+        res.status(403).send({
+          error: 'you do not have access to this resource'
+        })
+      } else {
+        req.user = user
+        next()
+      }
+    }, 1000)
   })(req, res, next)
 }
