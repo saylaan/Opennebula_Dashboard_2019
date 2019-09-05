76472e2bd62da7716a4aeb6bcdffa5042f638913
root <root@dspp.al-enterprise.com>
2019-09-06 00:05:59 +0200

    stable build

--- server/src/policies/isAuthenticated.js
+++ server/src/policies/isAuthenticated.js
@@ -1,10 +1,7 @@
 const passport = require('passport')
 
 module.exports = async function (req, res, next) {
-  await setTimeout((err, data) => {
-    console.log('Time out before authenticated....')
     await passport.authenticate('jwt', async function (err, user) {
-      console.log('user', user)
       if (err || !user) {
         res.status(403).send({
           error: 'you do not have access to this resource'
@@ -14,5 +11,4 @@ module.exports = async function (req, res, next) {
         next()
       }
     })(req, res, next)
-  }, 1000)
 }
